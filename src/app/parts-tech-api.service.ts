5270a971186bc133ea3287e226d0cd3992b4d007
Dennis Marchuk <dvmarchuk4@gmail.com>
2021-06-18 16:56:22 +0300

    Project for T&T repair to manage shop supplies

--- src/app/parts-tech-api.service.ts
+++ src/app/parts-tech-api.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from '@angular/core';
-import {HttpClient, HttpInterceptor, HttpHeaders, HttpParams} from '@angular/common/http';
+import {HttpClient, HttpHeaderResponse, HttpInterceptor, HttpHeaders, HttpParams} from '@angular/common/http';
 import { Observable, throwError } from 'rxjs';
 import { catchError, retry } from 'rxjs/operators';
 import * as data from './auth.json';
+//import {Headers} from 'angular2/http';
 
 
 
@@ -17,25 +18,52 @@ export class PartsTechAPIService {
 
 
 
-  url = ('https://api.partstech.com/oauth/access');
+  url = 'https://api.partstech.com/oauth/access';
 
-  // @ts-ignore
-  // options: {
-  //   headers: 'id': (dmarchuk), 'key': (1be7bc0967eb478b8fa3ea350cdf0b44)
-  // };
 
-  // "credentials": {
-  //   "user": {
-  //     "id": "dmarchuk",
-  //     "key": "1be7bc0967eb478b8fa3ea350cdf0b44"
-  //   }
-  // }
   // tslint:disable-next-line:typedef
-
   getAPI() {
     const headers = new HttpHeaders();
-    this.http.post(this.url, {headers}).subscribe();
-  }
+    headers.append('Content-type', 'application/json');
 
-  // Authorization: Bearer <accessToken>
+    const payload = {
+      accessType: 'user',
+      credentials: {
+      user: {
+        id: 'dmarchuk',
+          key: '1be7bc0967eb478b8fa3ea350cdf0b44'
+      },
+      partner: {
+        id: 'dmarchuk',
+          key: 'myrbar-nocqYc-0hejce'
+      }
+    }};
+
+    this.http.post(this.url, JSON.stringify(payload), {headers}).subscribe();
+
+
+
+  }
 }
+
+
+
+
+
+
+// Authorization: Bearer <accessToken>
+
+
+// @ts-ignore
+// options: {
+//   headers: 'id': (dmarchuk), 'key': (1be7bc0967eb478b8fa3ea350cdf0b44)
+// };
+
+// "credentials": {
+//   "user": {
+//     "id": "dmarchuk",
+//     "key": "1be7bc0967eb478b8fa3ea350cdf0b44"
+//   }
+// }
+// tslint:disable-next-line:typedef
+
